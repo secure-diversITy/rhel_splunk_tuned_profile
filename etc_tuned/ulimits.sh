43ac5e559e31afbd54cd68fd9317916754500e70
TF <mail@sedi.one>
2020-11-26 16:12:27 +0100

    use generic init log file name

--- etc_tuned/ulimits.sh
+++ etc_tuned/ulimits.sh
@@ -11,21 +11,21 @@ CRIBL_USER=clintsharp
 
 start() {
     su - $CRIBL_USER
-    echo "$(date) - logstream init start: nofiles soft $(ulimit -Sn)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init start: nofiles hard $(ulimit -Hn)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init start: no processes $(ulimit -u)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init start: max file size $(ulimit -f)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init start: data segment size $(ulimit -d)" >> $CRIBLH/log/splunk-init-ulimit.log
+    echo "$(date) - logstream init start: nofiles soft $(ulimit -Sn)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init start: nofiles hard $(ulimit -Hn)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init start: no processes $(ulimit -u)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init start: max file size $(ulimit -f)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init start: data segment size $(ulimit -d)" >> $CRIBLH/log/init-ulimit.log
     ulimit -Hn 65536
     ulimit -Sn 32768
     ulimit -u 32000
     ulimit -f unlimited
     ulimit -d unlimited
-    echo "$(date) - logstream init end : nofiles soft $(ulimit -Sn)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init end : nofiles hard $(ulimit -Hn)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init end : no processes $(ulimit -u)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init end : max file size $(ulimit -f)" >> $CRIBLH/log/splunk-init-ulimit.log
-    echo "$(date) - logstream init end : data segment size $(ulimit -d)" >> $CRIBLH/log/splunk-init-ulimit.log
+    echo "$(date) - logstream init end : nofiles soft $(ulimit -Sn)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init end : nofiles hard $(ulimit -Hn)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init end : no processes $(ulimit -u)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init end : max file size $(ulimit -f)" >> $CRIBLH/log/init-ulimit.log
+    echo "$(date) - logstream init end : data segment size $(ulimit -d)" >> $CRIBLH/log/init-ulimit.log
     return 0
 }
 
